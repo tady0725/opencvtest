3c305e443fe43356658d70ff29f9b150842da516
tady <wa201267@gmail.com.tw>
2021-07-26 14:10:00 +0800

    s2

--- test3.py
+++ test3.py
@@ -1,8 +1,10 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-
+# read
 img = cv.imread('./sky.jpg')
+# img show
+img2 = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 plt.imshow(img, interpolation='bicubic')
 plt.xticks([]), plt.yticks([])  # 隐藏 X 和 Y 轴的刻度值
 plt.show()
